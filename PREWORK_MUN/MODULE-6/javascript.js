1585c026df5e441694344adb20d27f29fcbcacd3
Muneeb Ali <muneebbwp@gmail.com>
2020-09-03 21:10:02 -0400

    adding all my prework for BootCamp

---
+++ PREWORK_MUN/MODULE-6/javascript.js
@@ -0,0 +1,19 @@
+document.getElementById("button1").addEventListener("click", function(){
+ document.getElementById("box") .style="height:250px; width:250px; background-color:orange" });
+
+ document.getElementById("button2").addEventListener("click", function(){
+    document.getElementById("box") .style="height:150px; width:150px; background-color:blue" });
+
+    document.getElementById("button3").addEventListener("click", function(){
+        document.getElementById("box") .style="height:250px; width:250px; background-color:orange; opacity:0.5" });
+
+        document.getElementById("button4").addEventListener("click", function(){
+            document.getElementById("box") .style="height:150px; width:150px; background-color:orange" });
+
+
+
+
+
+
+        
+ 
